483c7c2f6b30b135ac595fa01bc7d8b0ecc5ef0e
hemanthrishee <88771647+hemanthrishee@users.noreply.github.com>
2025-04-21 14:00:10 +0530

    update user model to have created at and updated at fields

--- authentication/models.py
+++ authentication/models.py
@@ -5,3 +5,8 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
